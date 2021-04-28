09abdd047b7b1eeb04cee117101cc5920e76b17c
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-28 14:45:14 +0800

    adding Add Photo in Sign Up

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 import {Header} from '../../components';
 import {TextInput, Gap, Button} from '../../components';
@@ -9,6 +9,14 @@ function SignUp({navigation}) {
     <View style={styles.container}>
       <Header title="Sign Up" onBack={() => navigation.goBack()} />
       <View style={styles.contentWrapper}>
+        <View style={styles.avatarWrapper}>
+          <View style={styles.border}>
+            <View style={styles.addPhoto}>
+              <Text style={styles.textAddPhoto}>Add Photo</Text>
+            </View>
+          </View>
+        </View>
+
         <TextInput label="Full Name" placeholder="Enter your full name" />
         <Gap height={16} />
         <TextInput
@@ -35,5 +43,33 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 24,
     backgroundColor: 'white',
+  },
+  addPhoto: {
+    backgroundColor: '#f0f0f0',
+    width: 90,
+    height: 90,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 90,
+  },
+  textAddPhoto: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 13,
+    maxWidth: 40,
+    textAlign: 'center',
+  },
+
+  border: {
+    width: 110,
+    height: 110,
+    borderRadius: 110,
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderStyle: 'dashed',
+  },
+  avatarWrapper: {
+    alignItems: 'center',
+    marginVertical: 26,
   },
 });
