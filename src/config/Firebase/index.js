12f6fb538eead280c12375abd7c83789462f434f
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-29 14:19:17 +0800

    adding firebase

---
+++ src/config/Firebase/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBbpct0hIaeZGS7WR1m48T8IKnoVp_kY1M',
+  authDomain: 'moneytracker-9473e.firebaseapp.com',
+  databaseURL: 'https://moneytracker-9473e-default-rtdb.firebaseio.com',
+  projectId: 'moneytracker-9473e',
+  storageBucket: 'moneytracker-9473e.appspot.com',
+  messagingSenderId: '261335311170',
+  appId: '1:261335311170:web:a991f6e4d362acc929dedb',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
